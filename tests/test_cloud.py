f38d8d77c52d776f7db35b9bc15075b386187314
kiefersv <reronex@gmail.com>
2025-03-20 10:56:03 -0500

    added test

--- tests/test_cloud.py
+++ tests/test_cloud.py
@@ -22,3 +22,17 @@ def test_virga_cloud():
 
     assert np.isclose(np.sum(all_out['condensate_mmr']), 6.163947994805619e-05)
 
+def test_virga_fsed():
+    # Note: constant fsed is tested in test_virga_single_cloud()
+
+    # TODO: add test for exp
+
+    # fsed array
+    a = jdi.Atmosphere(['MnS'], fsed=np.linspace(2, 0.1, 53), param='array')
+    a.gravity(gravity=7.460, gravity_unit=u.Unit('m/(s**2)'))
+    a.ptk(df=jdi.hot_jupiter())
+    all_out = jdi.compute(a, directory='.')
+    assert np.isclose(np.sum(all_out['condensate_mmr']), 5.7569695488823735e-05)
+    assert np.isclose(np.sum(all_out['mean_particle_r']), 250.514075712179)
+    assert np.isclose(np.sum(np.nan_to_num(all_out['column_density'])), 643.8594768055566)
+
