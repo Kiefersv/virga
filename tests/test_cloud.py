10f59334528edbba76da855766a523e87e08e1d8
kiefersv <reronex@gmail.com>
2025-04-01 08:16:50 -0500

    make ready for size_dist merge

--- tests/test_cloud.py
+++ tests/test_cloud.py
@@ -14,14 +14,38 @@ def test_mie_database():
     assert np.isclose(np.sum(asym), 1093.2539398130693)
 
 def test_virga_single_cloud():
-    # single cloud atmosphere
-    a = jdi.Atmosphere(['MnS'], fsed=1)
+    # initialise atmosphere
+    a = jdi.Atmosphere(['MnS'], fsed=1, mh=1, mmw=2.2)
     a.gravity(gravity=7.460, gravity_unit=u.Unit('m/(s**2)'))
     a.ptk(df=jdi.hot_jupiter())
-    all_out = jdi.compute(a, directory='.')
+    # calculate cloud profile using og_solver
+    all_out = jdi.compute(a, as_dict=True, directory='.')
+    # check outputs
     assert np.isclose(np.sum(all_out['condensate_mmr']), 6.163947994805619e-05)
-    assert np.isclose(np.sum(all_out['mean_particle_r']), 164.3272526232421)
-    assert np.isclose(np.sum(np.nan_to_num(all_out['column_density'])), 4950.549205376381)
+    assert np.isclose(np.sum(all_out['mean_particle_r']), 213.68473817971767)
+    assert np.isclose(np.sum(all_out['droplet_eff_r']), 710.2622567489823)
+    assert np.isclose(np.sum(all_out['column_density']), 2184.11033113616)
+    assert np.isclose(np.sum(all_out['single_scattering']), 5752.215299269361)
+    assert np.isclose(np.sum(all_out['asymmetry']), 3736.8968648397567)
+    assert np.isclose(np.sum(all_out['opd_by_gas']), 0.9849945642140122)
+    assert np.isclose(np.sum(all_out['opd_per_layer']), 0.010309205846656488)
+    
+def test_virga_direct_solver():
+    # initialise atmosphere
+    a = jdi.Atmosphere(['MnS'], fsed=1, mh=1, mmw=2.2)
+    a.gravity(gravity=7.460, gravity_unit=u.Unit('m/(s**2)'))
+    a.ptk(df=jdi.hot_jupiter())
+    # calculate cloud profile using og_solver
+    all_out = jdi.compute(a, as_dict=True, directory='.', og_solver=False)
+    # check outputs
+    assert np.isclose(np.sum(all_out['condensate_mmr']), 1.843949389772658e-05)
+    assert np.isclose(np.sum(all_out['mean_particle_r']), 720.4830441320369)
+    assert np.isclose(np.sum(all_out['droplet_eff_r']), 2394.7986048690545)
+    assert np.isclose(np.sum(all_out['column_density']), 51.23056414336807)
+    assert np.isclose(np.sum(all_out['single_scattering']), 5743.974745378395)
+    assert np.isclose(np.sum(all_out['asymmetry']), 3910.185884007504)
+    assert np.isclose(np.sum(all_out['opd_by_gas']), 0.2613196396359124)
+    assert np.isclose(np.sum(all_out['opd_per_layer']), 0.0007309227958875775)
 
 def test_virga_size_distribution():
     # calculate different size distributions
@@ -32,18 +56,21 @@ def test_virga_size_distribution():
     a.ptk(df=jdi.hot_jupiter())
     all_out = jdi.compute(a, as_dict=True, directory='.')
     assert np.isclose(np.sum(all_out['droplet_eff_r']), 940.9847777172278)
+    assert np.isclose(np.sum(all_out['opd_per_layer']), 0.001924747698604046)
     # exponential distribution
     a = jdi.Atmosphere(['MnS'], fsed=1, mh=1, mmw=2.2, size_distribution='exponential')
     a.gravity(gravity=7.460, gravity_unit=u.Unit('m/(s**2)'))
     a.ptk(df=jdi.hot_jupiter())
     all_out = jdi.compute(a, as_dict=True, directory='.')
     assert np.isclose(np.sum(all_out['droplet_eff_r']), 776.5807857795131)
+    assert np.isclose(np.sum(all_out['opd_per_layer']), 0.003930891126005702)
     # monodisperse distributions
     a = jdi.Atmosphere(['MnS'], fsed=1, mh=1, mmw=2.2, size_distribution='monodisperse')
     a.gravity(gravity=7.460, gravity_unit=u.Unit('m/(s**2)'))
     a.ptk(df=jdi.hot_jupiter())
     all_out = jdi.compute(a, as_dict=True, directory='.')
     assert np.isclose(np.sum(all_out['droplet_eff_r']), 1195.0939223236237)
+    assert np.isclose(np.sum(all_out['opd_per_layer']), 0.0005444735981582805)
 
 def test_virga_mixed_cloud():
     # mixed cloud atmosphere
